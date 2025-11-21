8ea177ce4f544479524f15dbe5e183bf45c543c8
Ana R. Carvalho <anarcarvalho93@gmail.com>
2025-11-20 23:29:46 -0300

    Atualizado

--- auth/app/policies/article_policy.rb
+++ auth/app/policies/article_policy.rb
@@ -2,26 +2,33 @@ class ArticlePolicy < ApplicationPolicy
   class Scope < Scope
     # NOTE: Be explicit about which records you allow access to!
     def resolve
-      scope.all
+      scope.alls
     end
   end
+
+    def new?
+      create?
+    end
+    
+    def create?
+      user&.has_role? :write
+    end
 
     def edit? 
       update?
     end
-    
+
     def update?
       # se é admin
       # user&.admin?
       # se é dono do artigo
-      user&.admin? || user&.id == record.user_id
+      user&.has_role? :editor || user&.id == record.user_id
     end
 
     def destroy?
       # se é admin
       # user&.admin?
       # se é dono do artigo
-      user&.has_role?(:admin) || user&.id == record.user_id
+      user&.admin? || user&.id == record.user_id
     end
 end
-
--- auth/db/schema.rb
+++ auth/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2025_11_20_182439) do
+ActiveRecord::Schema[8.1].define(version: 2025_11_20_201609) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -34,6 +34,7 @@ ActiveRecord::Schema[8.1].define(version: 2025_11_20_182439) do
   end
 
   create_table "users", force: :cascade do |t|
+    t.boolean "admin"
     t.datetime "created_at", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
